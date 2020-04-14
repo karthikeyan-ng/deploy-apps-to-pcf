0e28087eb75166d5a01405648841d56b6d324364
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-14 20:36:49 +0530

    Added Simple HelloController to test REST API.

---
+++ 1-hello-world-service/src/main/java/com/techstack/pcf/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.techstack.pcf.controller;
+
+import com.techstack.pcf.controller.api.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
---
+++ 1-hello-world-service/src/main/java/com/techstack/pcf/controller/api/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.techstack.pcf.controller.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+}
